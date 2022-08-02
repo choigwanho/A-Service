c6b988795dd079ba285707d209afb6ade7421a9c
dev-choee <rhksgh99@gmail.com>
2022-08-02 14:04:09 +0900

    - JPA 적용하기 완료 - 다음 진도 CRUD 구현

---
+++ src/test/java/com/choee/service/springboot/web/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,49 @@
+package com.choee.service.springboot.web.domain.posts;
+
+import com.choee.service.springboot.domain.posts.Posts;
+import com.choee.service.springboot.domain.posts.PostsRepository;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기() {
+        //given
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        // 빌더를 활용한 빌드 패턴
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author("rhksh99@gmail.com")
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+}
